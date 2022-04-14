0ca1579535d715ec13944dc23bc66e716dbeb620
Jonathan Reyes <jonathan.reyes2777gmail.com>
2022-04-14 16:18:27 -0500

    feat: add method to draw lines horizontal

--- canvas_controller.py
+++ canvas_controller.py
@@ -3,6 +3,8 @@ class CanvasController:
     def __init__(self, wide_canvas: int, height_canvas: int):
         self.wide_canvas = wide_canvas
         self.height_canvas = height_canvas
+        self.canvas = None
+        self.list_lines_canvas = []
         self._create_board_canvas()
 
     @property
@@ -29,7 +31,26 @@ class CanvasController:
             raise Exception('Value height should more that 0')
         self._height_canvas = value
 
+    def _draw_canvas(self):
+        self.canvas = ''
+        for line in self.list_lines_canvas:
+            self.canvas += line + '\n'
+
     def _create_board_canvas(self):
         top = '-' * (self.wide_canvas + 2)
-        height = ('\n|' + ' ' * self.wide_canvas + '|') * self.height_canvas
-        self.canvas = top + height + '\n' + top
+        self.list_lines_canvas.append(top)
+        self.list_lines_canvas.append(top)
+        for x in range(0, self.height_canvas):
+            self.list_lines_canvas.insert(-1, f'|{self.wide_canvas * " "}|')
+        self._draw_canvas()
+
+    def draw_line(self, x1, y1, x2, y2):
+        line_is = None
+        if x1 == x2:
+            line_is = 'horizontal'
+        elif y1 == y2:
+            line_is = 'vertical'
+        long_line = x2 - (x1-1)
+        stop = x1 + long_line
+        self.list_lines_canvas[y2] = self.list_lines_canvas[y2][:x1] + ('x' * long_line) + self.list_lines_canvas[y2][stop:]
+        self._draw_canvas()
--- test/test_canvas_controller.py
+++ test/test_canvas_controller.py
@@ -12,7 +12,9 @@ from canvas_controller import CanvasController
                   )
 def test_instance_canvas_controller_error(data_test):
     with pytest.raises(Exception) as error:
-        canvas_controller = CanvasController(data_test['wide'], data_test['height'])
+        CanvasController(
+            wide_canvas=data_test['wide'],
+            height_canvas=data_test['height'])
     assert str(error.value) == data_test['response']
 
 
@@ -22,16 +24,56 @@ def test_instance_canvas_controller_error(data_test):
                                              '|                    |\n'
                                              '|                    |\n'
                                              '|                    |\n'
-                                             '----------------------'}),
+                                             '----------------------\n'}),
                                 ({'wide': 4, 'height': 4,
                                  'response': '------\n'
                                              '|    |\n'
                                              '|    |\n'
                                              '|    |\n'
                                              '|    |\n'
-                                             '------'}),
+                                             '------\n'}),
                                 )
                   )
 def test_create_canvas(data_test):
-    canvas_controller = CanvasController(data_test['wide'], data_test['height'])
+    canvas_controller = CanvasController(
+        wide_canvas=data_test['wide'],
+        height_canvas=data_test['height'])
     assert canvas_controller.canvas == data_test['response']
+
+
+@mark.parametrize('data_test', (({'x1': 1, 'y1': 2, 'x2': 6, 'y2': 2,
+                                  'response': '----------------------\n'
+                                              '|                    |\n'
+                                              '|xxxxxx              |\n'
+                                              '|                    |\n'
+                                              '|                    |\n'
+                                              '----------------------\n'}),
+                                ({'x1': 1, 'y1': 1, 'x2': 6, 'y2': 1,
+                                  'response': '----------------------\n'
+                                              '|xxxxxx              |\n'
+                                              '|                    |\n'
+                                              '|                    |\n'
+                                              '|                    |\n'
+                                              '----------------------\n'}),
+                                ({'x1': 8, 'y1': 1, 'x2': 13, 'y2': 1,
+                                  'response': '----------------------\n'
+                                              '|       xxxxxx       |\n'
+                                              '|                    |\n'
+                                              '|                    |\n'
+                                              '|                    |\n'
+                                              '----------------------\n'}),
+                                ({'x1': 17, 'y1': 1, 'x2': 20, 'y2': 1,
+                                  'response': '----------------------\n'
+                                              '|                xxxx|\n'
+                                              '|                    |\n'
+                                              '|                    |\n'
+                                              '|                    |\n'
+                                              '----------------------\n'})
+
+                                )
+                  )
+def test_draw_line(data_test):
+    canvas_controller = CanvasController(20, 4)
+    canvas_controller.draw_line(x1=data_test['x1'], y1=data_test['y1'], x2=data_test['x2'], y2=data_test['y2'])
+    assert canvas_controller.canvas == data_test['response']
+
