9f6050ad565ba8565aa851c0a9128d887318269b
Jonathan Reyes <jonathan.reyes2777gmail.com>
2022-04-14 11:47:28 -0500

    feat: add raises error to instance class

--- canvas_controller.py
+++ canvas_controller.py
@@ -5,6 +5,30 @@ class CanvasController:
         self.height_canvas = height_canvas
         self._create_board_canvas()
 
+    @property
+    def wide_canvas(self):
+        return self._wide_canvas
+
+    @wide_canvas.setter
+    def wide_canvas(self, value):
+        if not type(value) == int:
+            raise Exception('Value wide should Integer')
+        if not value > 0:
+            raise Exception('Value wide should more that 0')
+        self._wide_canvas = value
+
+    @property
+    def height_canvas(self):
+        return self._height_canvas
+
+    @height_canvas.setter
+    def height_canvas(self, value):
+        if not type(value) == int:
+            raise Exception('Value height should Integer')
+        if not value > 0:
+            raise Exception('Value height should more that 0')
+        self._height_canvas = value
+
     def _create_board_canvas(self):
         top = '-' * (self.wide_canvas + 2)
         height = ('\n|' + ' ' * self.wide_canvas + '|') * self.height_canvas
--- test/test_canvas_controller.py
+++ test/test_canvas_controller.py
@@ -1,6 +1,19 @@
+import pytest
 from pytest import mark
 
 from canvas_controller import CanvasController
+
+
+@mark.parametrize('data_test', (({'wide': 'A', 'height': 3, 'response': 'Value wide should Integer'}),
+                                ({'wide': 1, 'height': 'A', 'response': 'Value height should Integer'}),
+                                ({'wide': -10, 'height': 10, 'response': 'Value wide should more that 0'}),
+                                ({'wide': 10, 'height': -10, 'response': 'Value height should more that 0'}),
+                                )
+                  )
+def test_instance_canvas_controller_error(data_test):
+    with pytest.raises(Exception) as error:
+        canvas_controller = CanvasController(data_test['wide'], data_test['height'])
+    assert str(error.value) == data_test['response']
 
 
 @mark.parametrize('data_test', (({'wide': 20, 'height': 4,
@@ -22,6 +35,3 @@ from canvas_controller import CanvasController
 def test_create_canvas(data_test):
     canvas_controller = CanvasController(data_test['wide'], data_test['height'])
     assert canvas_controller.canvas == data_test['response']
-
-
-
