2e7c471583a3119843fcb0279b9085625ac1002e
Jonathan Reyes <jonathan.reyes2777gmail.com>
2022-04-14 10:30:50 -0500

    feat: create method to create board canvas and testing

--- canvas_controller.py
+++ canvas_controller.py
@@ -5,4 +5,7 @@ class CanvasController:
         self.height_canvas = height_canvas
         self._create_board_canvas()
 
-        
+    def _create_board_canvas(self):
+        top = '-' * (self.wide_canvas + 2)
+        height = ('\n|' + ' ' * self.wide_canvas + '|') * self.height_canvas
+        self.canvas = top + height + '\n' + top
--- test/test_canvas_controller.py
+++ test/test_canvas_controller.py
@@ -1,11 +1,27 @@
+from pytest import mark
+
 from canvas_controller import CanvasController
 
 
-def test_create_canvas():
-    canvas_controller = CanvasController(20, 4)
-    assert canvas_controller.canvas == '---------------------------\n' \
-                                       '|                         |\n' \
-                                       '|                         |\n' \
-                                       '|                         |\n' \
-                                       '|                         |\n' \
-                                       '---------------------------'
+@mark.parametrize('data_test', (({'wide': 20, 'height': 4,
+                                 'response': '----------------------\n'
+                                             '|                    |\n'
+                                             '|                    |\n'
+                                             '|                    |\n'
+                                             '|                    |\n'
+                                             '----------------------'}),
+                                ({'wide': 4, 'height': 4,
+                                 'response': '------\n'
+                                             '|    |\n'
+                                             '|    |\n'
+                                             '|    |\n'
+                                             '|    |\n'
+                                             '------'}),
+                                )
+                  )
+def test_create_canvas(data_test):
+    canvas_controller = CanvasController(data_test['wide'], data_test['height'])
+    assert canvas_controller.canvas == data_test['response']
+
+
+
