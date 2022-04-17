dbf0bc9d1c72482535041e7405bd63dd48091777
Jonathan Reyes <jonathan.reyes2777gmail.com>
2022-04-17 09:55:07 -0500

    chore: add documentation to MainApp

--- main.py
+++ main.py
@@ -4,20 +4,55 @@ from canvas_controller import CanvasController
 
 
 class MainApp:
-    def __init__(self):
+    """
+    This is class reads input file to process data and execute order to finally write in output file
+    Attributes:
+        _input_file_name (str): file name with extension .txt where extract orders to exec in canvas board
+        _output_file_name (str): file name where with extension .txt write all orders executed in canvas board
+        _input_lines (list): list where save lines to execute orders in canvas board
+        _output_lines (list) list where save lines to write in output file after executed all order in canvas board
+
+    Returns:
+        Create a file with name _output_file_name
+
+    Example:
+        app = MainApp(input_file_name='input.txt', output_file_name='output.txt')
+        app.execute_file()
+
+    """
+
+    def __init__(self, input_file_name: str, output_file_name: str):
+        """
+                The constructor for MainApp class.
+
+                Parameters:
+                    input_file_name (str): file name with extension .txt where extract orders to
+                    execute in canvas board
+                    output_file_name (str): file name where with extension .txt write all orders
+                    executed in canvas board
+                """
+        self._input_file_name = input_file_name
+        self._output_file_name = output_file_name
         self._input_lines = None
         self._output_lines = None
+        self._read_file()
 
     def _read_file(self):
-        with open(sys.argv[1], 'r') as file:
+        with open(self._input_file_name, 'r') as file:
             self._input_lines = file.readlines()
 
     def _write_file(self):
-        with open(sys.argv[2], 'w') as file:
+        with open(self._output_file_name, 'w') as file:
             file.writelines(self._output_lines)
 
     def execute_file(self):
-        self._read_file()
+        """
+                The function execute all order in _input_lines and write _output_lines after execute all
+                order in canvas board.
+
+                Returns:
+                    Create a file with name _output_file_name
+         """
         line = self._input_lines[0].split()
         if line[0] != 'C':
             raise Exception('Is need first create Canvas board, The first line should be (C {value_x} {value_y})')
@@ -47,4 +82,4 @@ class MainApp:
 
 
 if __name__ == '__main__':
-    MainApp().execute_file()
+    MainApp(sys.argv[1], sys.argv[2]).execute_file()
